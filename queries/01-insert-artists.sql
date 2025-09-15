4e4c566461833efd9204cf44d37ab98655e5da05
Melfreitass <melissa.g.freitas@aluno.senai.br>
2025-09-15 10:26:53 -0300

    feat: insere primeiros valores na tabela

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -10,3 +10,10 @@ CREATE TABLE artistas (
     ativo BOOLEAN DEFAULT TRUE
 ); 
 
+INSERT INTO artistas (nome, tipo, membros, genero_musical, pais_origem,ano_inicio, top_musica, ativo)
+VALUES
+('Morada', 'Banda', 5, 'Gospel', 'Brasil', 2009, 'É tudo sobre você', TRUE),
+('Fhop Music', 'Banda', 3, 'Gospel', 'Brasil', 2014, 'Colossenses e suas linhas de amor'),
+('Nesk Only', 'Cantor', 1, 'Trap Gospel', 'Brasil', 2019, 'Ah teu'),
+('Forrest Frank', 'Cantor', 1, 'Gospel', 'EUA', '2018', 'YOUR WAYS BETTER');
+
