877eb45d842070cfbd170f96c65d87e32a15253f
Melfreitass <melissa.g.freitas@aluno.senai.br>
2025-09-15 10:49:33 -0300

    feat: finaliza selects

--- queries/02-selects.sql
+++ queries/02-selects.sql
@@ -1,2 +1,11 @@
-SELECT *FROM artistas
+SELECT *FROM artistas;
+
 SELECT COUNT(*) AS total_artistas FROM artistas;
+
+SELECT nome, musica_mais_ouvida FROM artistas;
+
+SELECT nome, top_musica FROM artistas
+WHERE pais_origem = 'Brasil'
+
+SELECT nome, top_musica FROM artistas
+WHERE id = 3;
